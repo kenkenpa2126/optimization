a49ced6f7dec9a209ddf1eb3f2b45ef766c90e8a
kenkenpa2126 <kenkenpa2126@gmail.com>
2021-03-21 00:13:36 +0900

    ver 1.0

--- server_side.py
+++ server_side.py
@@ -9,17 +9,15 @@ import numpy as np
 import dimod
 import math
 from flask import Flask, request, jsonify
-
 from flask_cors import CORS
-
 
 game_id = {'0': '馬娘', '1': 'バンドリ', '2': '原神', '3': 'プロジェクトセカイ', '4': 'パズドラ'}
 
 # APIキーの設置
-CONSUMER_KEY = 'xxxx'
-CONSUMER_SECRET = 'xxxx'
-ACCESS_TOKEN = '8xxx'
-ACCESS_SECRET = 'xxxx'
+CONSUMER_KEY = 'xxxxx'
+CONSUMER_SECRET = 'xxxxx'
+ACCESS_TOKEN = 'xxxxx'
+ACCESS_SECRET = 'xxxxx'
 
 twitter = OAuth1Session(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
 url = "https://api.twitter.com/1.1/search/tweets.json"
@@ -53,11 +51,19 @@ def kitaichi(game_name):
     kitaichi = len(tweets) * gacha_percent
     return kitaichi
 
-def optimize(game_type, W, c, w):
+def optimize(game_type, W, c, w, min_count):
     N = len(w)
-    x_max = {k: int(W / v) for k, v in w.items()}
-    print("x_max: ")
-    print(x_max)
+    # x_max = {k: int(W / v) for k, v in w.items()}
+
+    offset_value = {x: min_count[x] * y for x, y in w.items()}
+    x_max = {k: int((W - sum(offset_value)) / v) for k, v in w.items()}
+
+    for k, v in x_max.items():
+        if v <= 0:
+            return "paramater error"
+
+    # print("x_max: ")
+    # print(x_max)
 
     x = pyq.Array([pyq.LogEncInteger('x_{}'.format(i), (0, x_max[i])) for i in x_max.keys()])
     y = pyq.LogEncInteger('y', (0, W))
@@ -92,7 +98,7 @@ def optimize(game_type, W, c, w):
 
     # gacha_count = {v: result_x[k] for k,v in game_type.items()}
     # print(gacha_count)
-    gacha_count = {game_id[v]: result_x[k] for k, v in game_type.items()}
+    gacha_count = {game_id[str(v)]: result_x[k] + min_count[k] for k, v in enumerate(game_type)}
     print(gacha_count)
 
     for k, v in gacha_count.items():
@@ -106,49 +112,48 @@ def optimize(game_type, W, c, w):
 app = Flask(__name__)
 CORS(app)
 
-
 @app.route('/', methods=['GET', 'POST'])
 def do_Post():
     gacha_bid = {'0': 347, '1': 340, '2': 320, '3': 315, '4': 717}
 
-    # mode = request['mode']
-    mode = 2
+    mode = request.json['mode']
     if mode == 0:
-        input_c = request['c']
+        input_c = request.json['c']
     elif mode == 1:
         input_c = gacha_percent_dic
     elif mode == 2:
         input_c = {k: kitaichi(k) for k, v in game_id.items()}
     else:
-        input_c = request['c']
+        return "paramater error"
 
-    # game_type = request['game']
-    # W = request['W']
+    min_count = request.json['min']
+    game_type = request.json['game']
+    W = request.json['moneyValue']
 
     print("input_c:")
     print(input_c)
-    # print("game_type:")
-    # print(game_type)
-    # print("W:")
-    # print(W)
+    print("game_type:")
+    print(game_type)
+    print("W:")
+    print(W)
 
-    game_type = {0:'0', 1:'1', 2:'2'}
+    # game_type = {0:'0', 1:'1', 2:'2'}
     # c = {'0':5, '1':1, '2':3 }
-    W = 3000
+    # W = 3000
 
     # Integer variable array
-    w = {k: gacha_bid[v] for k, v in game_type.items()}
-    c = {k: input_c[v] for k, v in game_type.items()}
+    w = {k: gacha_bid[str(v)] for k,v in enumerate(game_type)}
+    c = {k: input_c[str(v)] for k,v in enumerate(game_type)}
+    min_count = {k: min_count[str(v)] for k,v in enumerate(game_type)}
     print("w:")
     print(w)
     print("c:")
     print(c)
 
-    result = optimize(game_type, W, c, w)
+    result = optimize(game_type, W, c, w, min_count)
     resultstr = {k: str(v) for k, v in result.items()}
 
     return jsonify(resultstr)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
